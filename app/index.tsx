586775d2b078a6328f469a7ea0e65e1b5022c9ed
vinhng10 <qvinhnguyen17@gmail.com>
2024-09-25 00:48:55 +0300

    final flow

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
 import * as FileSystem from "expo-file-system";
 import { Image } from "expo-image";
 import EventSource from "react-native-sse";
+import cheerio from "react-native-cheerio";
 
 const CameraScreen = () => {
   const [permission, requestPermission] = useCameraPermissions();
@@ -15,8 +16,47 @@ const CameraScreen = () => {
   const [isTTS, setIsTTS] = useState(false);
   const [audioQueue, setAudioQueue] = useState<string[]>([]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [messages, setMessages] = useState<any[]>([]);
+  const [stop, setStop] = useState(true);
   const cameraRef = useRef(null);
   const router = useRouter();
+  const toolSchemas = [
+    {
+      type: "function",
+      function: {
+        name: "search",
+        description: "Perform a Google search given a query string.",
+        parameters: {
+          type: "object",
+          properties: {
+            query: {
+              type: "string",
+              description: "The search query to be used.",
+            },
+          },
+          required: ["query"],
+        },
+      },
+    },
+    {
+      type: "function",
+      function: {
+        name: "scrape",
+        description:
+          "Extract and return the text content from the webpage at the given URL.",
+        parameters: {
+          type: "object",
+          properties: {
+            url: {
+              type: "string",
+              description: "The URL of the webpage to scrape for text content.",
+            },
+          },
+          required: ["url"],
+        },
+      },
+    },
+  ];
 
   const parse = (data) => {
     const results: string[] = [];
@@ -35,138 +75,247 @@ const CameraScreen = () => {
     return results;
   };
 
-  const handleText = async () => {
+  const search = async (query: string) => {
     try {
-      // if (!cameraRef.current) return;
-      // // Take a picture and get the file URI
-      // const photo = await cameraRef.current.takePictureAsync({
-      //   base64: true,
-      // });
+      const results = [];
+      const params = new URLSearchParams({
+        key: "AIzaSyAv6tH86BlLoqfffajxG2nyJGLcrEZ2xMM",
+        cx: "96b297446bad847ad",
+        q: query,
+        start: "1",
+      });
 
-      // // Calculate the aspect ratio
-      // const aspectRatio = photo.width / photo.height;
+      // Make the HTTP GET request to the Google Custom Search JSON API
+      const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?${params}`
+      );
 
-      // // Calculate new dimensions
-      // let newWidth = 1200;
-      // let newHeight = 1200;
-      // if (aspectRatio > 1) {
-      //   newHeight = newWidth / aspectRatio;
-      // } else {
-      //   newWidth = newHeight * aspectRatio;
-      // }
+      const data = await response.json();
 
-      // const resizedPhoto = await manipulateAsync(
-      //   photo.uri,
-      //   [{ resize: { width: newWidth, height: newHeight } }],
-      //   { compress: 1, format: SaveFormat.JPEG }
-      // );
-      // setImage(resizedPhoto.uri);
+      const searchResults = data.items || [];
 
-      // // Create a new FormData object
-      // const formData = new FormData();
-      // formData.append("image", {
-      //   uri: resizedPhoto.uri,
-      //   type: "image/jpeg",
-      //   name: "image.jpg",
-      // });
+      // Extract search results and parse them
+      searchResults.slice(0, 3).forEach((searchItem) => {
+        results.push({
+          title: searchItem.title,
+          description: searchItem.snippet,
+          url: searchItem.link,
+        });
+      });
 
-      // // Make the POST request to your server
-      // const response = await fetch(
-      //   "https://api.bing.microsoft.com/v7.0/images/visualsearch?mkt=en-US",
-      //   {
-      //     method: "POST",
-      //     body: formData,
-      //     headers: {
-      //       "Ocp-Apim-Subscription-Key": "",
-      //     },
-      //   }
-      // );
-      // if (!response.ok) {
-      //   throw Error(`Error uploading image: ${response.statusText}`);
-      // }
-      // const data = await response.json();
-      // const results = parse(data);
-      const results = [
-        "Analyzing Kasper's vision - Inside The Vatican",
-        "Raffaello Sanzio's (Raphael) - Marriage of the Virgin. | Art day ...",
-        "Pinacoteca di Brera (Mailand) - Aktuelle 2019 - Lohnt es sich? (Mit fotos)",
-        "Moja Top-lista, czyli drugi spacer po Pinacoteca di Brera (post_71 ...",
-        "「ブレラ絵画館(ブレラ美術館)」完全ガイド – チケット予約・見どころ・行き方・混みぐあい",
-        "Pinacoteca di Brera (Milan, Italy): Top Tips Before You Go (with Photos ...",
-        "IMG_0228 The Marriage of the Virgin (1504), painting by Ra… | Flickr",
-        "A master visual catechist: Raphael and the Italian High Renaissance ...",
-        "Marriage of the Virgin after Raphael 'Raffaello Sanzio da Urbino', 1483 ...",
-        "The Circumcision | Marco Marzile, 1500, oil on canvas. | Brule Laker ...",
-      ];
-      // console.log(results.map((r, i) => `${i + 1}. ${r}`).join("\n"));
+      return results;
+    } catch (e) {
+      console.error(`Error making Google search: ${e}`);
+      return [];
+    }
+  };
 
-      // let messages = [
-      //   {
-      //     role: "system",
-      //     content:
-      //       "You are an esteemed art history professor with expertise in " +
-      //       "identifying artworks and interpreting their content. When " +
-      //       "presented with a list of visual search results, identify the " +
-      //       "actual artwork, including its title and artist. Then, describe " +
-      //       "the specific event or moment depicted in the piece. Use the " +
-      //       "tools if necessary to gather additional information to ensure " +
-      //       "your response is accurate and complete.",
-      //   },
-      //   {
-      //     role: "user",
-      //     content:
-      //       `Here is a list of visual search results of an artwork:\n\n${results}\n\n` +
-      //       `Identify the true artwork, then describe the specific event ` +
-      //       `or moment depicted in that artwork.`,
-      //   },
-      // ];
-      // let stop = false;
+  const scrape = async (url: string): Promise<string> => {
+    try {
+      // Send an HTTP GET request to the URL
+      const response = await fetch(url);
 
-      let text = "";
+      // Check if the request was successful (status code 200)
+      if (response.status !== 200) {
+        throw Error("Fetch URL failed");
+      }
 
-      const es = new EventSource(
-        "https://api.groq.com/openai/v1/chat/completions",
+      // Parse the HTML content of the page using cheerio
+      const data = await response.text();
+      const $ = cheerio.load(data);
+
+      // Extract the text content of the webpage and limit it to 2000 characters
+      const text = $("p")
+        .map((i, p) => $(p).text().trim())
+        .get()
+        .join("\n")
+        .slice(0, 2000);
+
+      return text;
+    } catch (e) {
+      console.error(`An error occurred: ${e}`);
+      return "";
+    }
+  };
+
+  const handleImage = async () => {
+    try {
+      if (!cameraRef.current) return;
+
+      // Take a picture and get the file URI
+      const photo = await cameraRef.current.takePictureAsync({
+        base64: true,
+      });
+
+      // Calculate the aspect ratio
+      const aspectRatio = photo.width / photo.height;
+
+      // Calculate new dimensions
+      let newWidth = 1000;
+      let newHeight = 1000;
+      if (aspectRatio > 1) {
+        newHeight = newWidth / aspectRatio;
+      } else {
+        newWidth = newHeight * aspectRatio;
+      }
+
+      const resizedPhoto = await manipulateAsync(
+        photo.uri,
+        [{ resize: { width: newWidth, height: newHeight } }],
+        { compress: 1, format: SaveFormat.JPEG }
+      );
+      setImage(resizedPhoto.uri);
+
+      // Create a new FormData object
+      const formData = new FormData();
+      formData.append("image", {
+        uri: resizedPhoto.uri,
+        type: "image/jpeg",
+        name: "image.jpg",
+      });
+
+      // Make the POST request to your server
+      const response = await fetch(
+        "https://api.bing.microsoft.com/v7.0/images/visualsearch?mkt=en-US",
         {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${""}`,
-          },
           method: "POST",
-          body: JSON.stringify({
-            model: "llama-3.1-8b-instant",
-            messages: [
-              {
-                role: "system",
-                content: "You are a helpful assistant.",
-              },
-              {
-                role: "user",
-                content: "Tell me a very short story in 3 sentences.",
-              },
-            ],
-            stream: true,
-          }),
-          pollingInterval: 0,
+          body: formData,
+          headers: {
+            "Ocp-Apim-Subscription-Key": "",
+          },
         }
       );
+      console.log(response);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const data = await response.json();
+      const results = parse(data);
+
+      let messages = [
+        {
+          role: "system",
+          content:
+            "You are an esteemed art history professor with expertise in " +
+            "identifying artworks and interpreting their content. When " +
+            "presented with a list of visual search results, identify the " +
+            "actual artwork, including its title and artist. Then, describe " +
+            "the specific event or moment depicted in the piece. Use the " +
+            "tools if necessary to gather additional information to ensure " +
+            "your response is accurate and complete.",
+        },
+        {
+          role: "user",
+          content:
+            `Here is a list of visual search results of an artwork:\n\n${results
+              .map((r, i) => `${i + 1}. ${r}`)
+              .join("\n")}\n\n` +
+            `Identify the true artwork, then describe the specific event ` +
+            `or moment depicted in that artwork.`,
+        },
+      ];
+
+      setMessages(messages);
+      setStop(false);
+    } catch (error) {
+      console.error("Image handling failed:", error);
+    }
+  };
+
+  const handleText = async () => {
+    try {
+      if (stop) return;
+
+      const tools = { search: search, scrape: scrape };
+
+      const es = new EventSource("https://api.openai.com/v1/chat/completions", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${""}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-4o-mini",
+          messages: messages,
+          tools: toolSchemas,
+          stream: true,
+        }),
+        pollingInterval: 0,
+      });
+
+      let text = "";
+      let toolCalls = {};
 
       es.addEventListener("open", () => {});
 
-      es.addEventListener("message", (event) => {
+      es.addEventListener("message", async (event) => {
         if (event.data === "[DONE]") {
           es.close();
           return;
         }
-        const content = JSON.parse(event.data).choices[0].delta.content;
-        switch (content) {
-          case ".":
-            setTextQueue((q) => [...q, text]);
-            text = "";
+        const choice = JSON.parse(event.data).choices[0];
+
+        switch (choice.finish_reason) {
+          case "stop":
+            console.log(messages);
+            setMessages([]);
+            setStop(true);
             break;
-          case undefined:
+
+          case "tool_calls":
+            const _messages = [
+              {
+                role: "assistant",
+                tool_calls: Object.values(toolCalls),
+              },
+            ];
+
+            for (const toolCall of Object.values(toolCalls)) {
+              const tool = toolCall.function;
+              const args = JSON.parse(tool.arguments);
+              const result = await tools[tool.name](...Object.values(args));
+
+              _messages.push({
+                role: "tool",
+                tool_call_id: toolCall.id,
+                content: JSON.stringify({ result: result }),
+              });
+            }
+
+            setMessages((m) => [...m, ..._messages]);
             break;
+
           default:
-            text += content;
+            const delta = choice.delta;
+            if (delta.tool_calls != null) {
+              for (const toolCall of delta.tool_calls) {
+                if (!toolCalls[toolCall.index]) {
+                  toolCalls[toolCall.index] = {
+                    id: toolCall.id,
+                    type: "function",
+                    function: {
+                      name: toolCall.function.name,
+                      arguments: toolCall.function.arguments,
+                    },
+                  };
+                } else {
+                  toolCalls[toolCall.index].function.arguments +=
+                    toolCall.function.arguments;
+                }
+              }
+            } else {
+              switch (delta.content) {
+                case ".":
+                  setTextQueue((q) => [...q, text]);
+                  text = "";
+                  break;
+                case undefined:
+                  break;
+                default:
+                  text += delta.content;
+              }
+            }
+            break;
         }
       });
 
@@ -274,6 +423,10 @@ const CameraScreen = () => {
   };
 
   useEffect(() => {
+    handleText();
+  }, [messages, stop]);
+
+  useEffect(() => {
     handleTTS();
   }, [textQueue, isTTS]);
 
@@ -317,7 +470,7 @@ const CameraScreen = () => {
     <View style={styles.container}>
       <CameraView ref={cameraRef} style={styles.camera} facing={"back"} />
       <View style={styles.bottomSection}>
-        <TouchableOpacity style={styles.roundButton} onPress={handleText}>
+        <TouchableOpacity style={styles.roundButton} onPress={handleImage}>
           <Text style={styles.text}>Take Photo</Text>
         </TouchableOpacity>
       </View>
